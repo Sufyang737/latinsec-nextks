2d8c0da2a78c6dbee37c679d3ab9cf3bc2637f83
Sufyan Ahmed <107362900+Sufyang737@users.noreply.github.com>
2024-09-26 18:54:07 -0300

    Update TestimonialCard.jsx

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -14,9 +14,6 @@ export const TestimonialCard = ({ data }) => {
   }
   return (
     <section className="bg-white px-16 py-6 relative ml-12">
-      <div className="w-24 h-24 rounded-full overflow-hidden absolute top-5 -left-12">
-        <Image src={image} alt="" className="object-cover" />
-      </div>
       <div>
         <h4 className="font-bold text-2xl text-[#252B42]">{name}</h4>
         <div className="text-[#F3CD03] text-xl">{stars}</div>
